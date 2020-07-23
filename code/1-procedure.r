01c77b45689b84ea0e5e4d36d36e33d5e08f787c
Ricardo Hortelano <ricardo.hortelano@gmail.com>
2020-07-23 12:56:17 +0200

    add leave-one-out cv

--- code/1-procedure.r
+++ code/1-procedure.r
@@ -28,7 +28,7 @@ df = tps$df
 
 df = perform_procrustes(df, x_coord_names, y_coord_names, plot_procrustes=FALSE)
 
-res = test_svmRadial(df, c(x_procrustes_coord_names, y_procrustes_coord_names), data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, c(x_procrustes_coord_names, y_procrustes_coord_names), data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 
@@ -39,7 +39,7 @@ df = tps$df
 gpa = perform_procrustes(df, x_coord_names, y_coord_names, plot_procrustes=FALSE, return_object=TRUE)
 df = remove_allometric_shape_effect(df, gpa)
 
-res = test_svmRadial(df, c(x_allom_coord_names, y_allom_coord_names), data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, c(x_allom_coord_names, y_allom_coord_names), data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Procrustes -> Remove Colinear -> SVM
@@ -49,7 +49,7 @@ df = tps$df
 df = perform_procrustes(df, x_coord_names, y_coord_names, plot_procrustes=FALSE)
 (non_colinear_cols = get_noncolinear_columns(df, columns = c(x_procrustes_coord_names, y_procrustes_coord_names), max_percentage=0.96, plot_correlation=FALSE))
 
-res = test_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Procrustes -> allo -> Remove Colinear -> SVM
@@ -60,7 +60,7 @@ gpa = perform_procrustes(df, x_coord_names, y_coord_names, plot_procrustes=FALSE
 df = remove_allometric_shape_effect(df, gpa)
 (non_colinear_cols = get_noncolinear_columns(df, columns = c(x_allom_coord_names, y_allom_coord_names), max_percentage=0.96, plot_correlation=FALSE))
 
-res = test_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Distances -> PCA95 -> SVM
@@ -70,7 +70,7 @@ distances_raw = get_all_distances(df, x_coord_names, y_coord_names)
 distances_pca = perform_pca_and_filter(distances_raw, columns=NULL, max_explained_var=0.95)
 df = cbind(df, distances_pca)
 
-res = test_svmRadial(df, colnames(distances_pca), data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, colnames(distances_pca), data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Distances -> PCA99 -> SVM
@@ -80,7 +80,7 @@ distances_raw = get_all_distances(df, x_coord_names, y_coord_names)
 distances_pca = perform_pca_and_filter(distances_raw, columns=NULL, max_explained_var=0.99)
 df = cbind(df, distances_pca)
 
-res = test_svmRadial(df, colnames(distances_pca), data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, colnames(distances_pca), data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Distances -> PCA999 -> SVM
@@ -90,7 +90,7 @@ distances_raw = get_all_distances(df, x_coord_names, y_coord_names)
 distances_pca = perform_pca_and_filter(distances_raw, columns=NULL, max_explained_var=0.999)
 df = cbind(df, distances_pca)
 
-res = test_svmRadial(df, colnames(distances_pca), data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, colnames(distances_pca), data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Distances -> Remove Colinear96 -> SVM
@@ -100,7 +100,7 @@ distances_raw = get_all_distances(df, x_coord_names, y_coord_names)
 (non_colinear_cols = get_noncolinear_columns(distances_raw, columns = NULL, max_percentage=0.96, plot_correlation=FALSE))
 df = cbind(df, distances_raw)
 
-res = test_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Distances -> Remove Colinear99 -> SVM
@@ -110,7 +110,7 @@ distances_raw = get_all_distances(df, x_coord_names, y_coord_names)
 (non_colinear_cols = get_noncolinear_columns(distances_raw, columns = NULL, max_percentage=0.99, plot_correlation=FALSE))
 df = cbind(df, distances_raw)
 
-res = test_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 #### Distances -> SVM
@@ -119,7 +119,7 @@ df = tps$df
 distances_raw = get_all_distances(df, x_coord_names, y_coord_names)
 df = cbind(df, distances_raw)
 
-res = test_svmRadial(df, colnames(distances_raw), data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, colnames(distances_raw), data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 ######## Procrustes -> Distances -> Remove Colinear -> SVM
@@ -131,7 +131,7 @@ distances_procrustes = get_all_distances(df, x_procrustes_coord_names, y_procrus
 (non_colinear_cols = get_noncolinear_columns(distances_procrustes, columns = NULL, max_percentage=0.96, plot_correlation=FALSE))
 df = cbind(df, distances_procrustes)
 
-res = test_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 ######## Procrustes -> allo -> Distances -> Remove Colinear -> SVM
@@ -144,9 +144,11 @@ distances_procrustes = get_all_distances(df, x_allom_coord_names, y_allom_coord_
 (non_colinear_cols = get_noncolinear_columns(distances_procrustes, columns = NULL, max_percentage=0.96, plot_correlation=FALSE))
 df = cbind(df, distances_procrustes)
 
-res = test_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
+res = test_LOOCV_svmRadial(df, non_colinear_cols, data_filter = data_filter, test_name=t_name)
 results = results %>% rbind(res)
 
 ##################################################################################
 ##################################################################################
+#diagnosis_table = df %>% data_filter %>% dplyr::select(diagnosis) %>% table
+#results = results %>% mutate(n_MAM=diagnosis_table['MAM'], n_NOR=diagnosis_table['NOR'], n_RIS=diagnosis_table['RIS'], n_SAM=diagnosis_table['SAM'])
 results

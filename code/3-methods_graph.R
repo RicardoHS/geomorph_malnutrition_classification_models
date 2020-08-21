68142a60b728f8e3d57b76dab36058a633c686cc
Ricardo Hortelano <ricardo.hortelano@gmail.com>
2020-08-21 12:36:20 +0200

    add method graph generation

---
+++ code/3-methods_graph.R
@@ -0,0 +1,21 @@
+library(igraph)
+
+methods_graph = graph_from_literal(DATA-+DISTANCES, 
+                                   DATA-+PROCRUSTES, 
+                                   PROCRUSTES-+DISTANCES,
+                                   PROCRUSTES-+ALLOMETRIC_SHAPE_REDUCTION,
+                                   PROCRUSTES-+CLASSIFICATION_MODEL,
+                                   PROCRUSTES-+COLINEARITY_TRIM,
+                                   PROCRUSTES-+PCA,
+                                   DISTANCES-+COLINEARITY_TRIM,
+                                   DISTANCES-+PCA,
+                                   DISTANCES-+CLASSIFICATION_MODEL,
+                                   PCA-+CLASSIFICATION_MODEL,
+                                   ALLOMETRIC_SHAPE_REDUCTION-+COLINEARITY_TRIM,
+                                   ALLOMETRIC_SHAPE_REDUCTION-+CLASSIFICATION_MODEL,
+                                   COLINEARITY_TRIM-+CLASSIFICATION_MODEL)
+
+
+plot(methods_graph)
+
+all_paths = all_simple_paths(methods_graph, from=1, to=5)
